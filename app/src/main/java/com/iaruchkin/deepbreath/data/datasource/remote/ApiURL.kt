4eff36f80dbfd1e3ba49c790cb860ac2bd6e4b3a
ruchkin <iaruchkin@gmail.com>
2024-07-28 01:10:26 +0300

    refactor wip
    add token interceptor

--- app/src/main/java/com/iaruchkin/deepbreath/data/datasource/remote/ApiURL.kt
+++ app/src/main/java/com/iaruchkin/deepbreath/data/datasource/remote/ApiURL.kt
@@ -1,6 +1,6 @@
 package com.iaruchkin.deepbreath.data.datasource.remote
 
 object ApiURL {
-    const val API_KEY = "8ec682b91914d1b760576870360c7217b6224104"
+    const val API_KEY_ENC = "OGVjNjgyYjkxOTE0ZDFiNzYwNTc2ODcwMzYwYzcyMTdiNjIyNDEwNA=="
     const val BASE_URL = "https://api.waqi.info/"
 }
--- app/src/main/java/com/iaruchkin/deepbreath/di/HeadersInterceptor.kt
+++ app/src/main/java/com/iaruchkin/deepbreath/di/HeadersInterceptor.kt
@@ -1,14 +1,15 @@
 package com.iaruchkin.deepbreath.di
 
-import com.iaruchkin.deepbreath.data.datasource.remote.ApiURL.API_KEY
+import android.util.Base64
+import com.iaruchkin.deepbreath.data.datasource.remote.ApiURL.API_KEY_ENC
 import okhttp3.Interceptor
 import okhttp3.Response
 
-
 class TokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val token = String(Base64.decode(API_KEY_ENC, Base64.DEFAULT))
         val url = chain.request().url.newBuilder()
-            .addQueryParameter("token", API_KEY)
+            .addQueryParameter("token", token)
             .build()
 
         val request = chain.request()
